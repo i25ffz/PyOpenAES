596d218221cce74e59375e53b1d594add2dd163b
付方助 <fangzhu.ffz@alibaba-inc.com>
2016-10-17 18:34:29 +0800

    Fix python test filename bug.

--- python/test.py
+++ python/test.py
@@ -1,5 +1,5 @@
 import openaes
 
 open('README.enc', 'w').write(openaes.encrypt(open('key_256').read(), open('README').read()))
-open('README.dec', 'w').write(openaes.decrypt(open('key_256').read(), open('README.aes').read()))
+open('README.dec', 'w').write(openaes.decrypt(open('key_256').read(), open('README.enc').read()))
 print open('README.dec').read()
